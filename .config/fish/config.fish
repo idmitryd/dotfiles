5b422279285ab75b1948d364f835a05b7e8fd372
Dmitry Dolzhikov <dolzhikov.d.a@gmail.com>
2023-10-22 17:48:25 +0300

    Move from gruvbox to kanagawa

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,7 @@
 direnv hook fish | source
 set -x SUDO_EDITOR (which nvim)
 fish_add_path ~/.local/bin
-# theme_gruvbox dark medium
-set_colors
+
+kanagawa
 
 alias config='/usr/bin/git --git-dir=$HOME/.mycfg/ --work-tree=$HOME'
